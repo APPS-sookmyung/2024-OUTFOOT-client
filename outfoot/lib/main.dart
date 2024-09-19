eda07a15f06ceb965b6450fd09eca2a4c5b05fde
codeJiwon <loopy0107@sookmyung.ac.kr>
2024-09-19 15:56:38 +0900

    [Feat/#19] 모달창 포함 UI 구현

--- outfoot/lib/main.dart
+++ outfoot/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:outfoot/screens/make_goal/make_personal_goal.dart';
 
 void main() {
   runApp(const MyApp());
@@ -31,7 +32,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MakePersonalGoalPage(),
     );
   }
 }
