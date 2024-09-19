ee042c3399b41e08ff64e05664c2e61e985e5924
codeJiwon <loopy0107@sookmyung.ac.kr>
2024-09-19 16:18:08 +0900

    [Feat/#20] 컬러코드 colors.dart에 정리

---
+++ outfoot/lib/colors/colors.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+//사용예시: Color(0xFFC8AA9B) -> mainBrownColor 대입
+const mainBrownColor = Color(0xFFC8AA9B);
+const mainBrownColor2 = Color(0xFFDFC4B6);
+const lightBrownColor = Color(0xFFF9F0EB);
+const darkBrownColor = Color(0xFF6D4C3A);
+
+const greyColor1 = Color(0xFF3E3E3E);
+const greyColor2 = Color(0xFF656565);
+const greyColor3 = Color(0xFF8B8B8B);
+const greyColor4 = Color(0xFFA4A4A4);
+const greyColor5 = Color(0xFFBDBDBD);
+const greyColor6 = Color(0xFFCFCFCF);
+const greyColor7 = Color(0xFFDFDFDF);
+const greyColor8 = Color(0xFFEDEDED);
+
+const lightColor = Color(0xFFF5F5F5);
+const lightMainColor = Color(0xFFFFFFFF);
+const greenColor = Color(0xFFC8EF2F);
+const yellowColor = Color(0xFFFFEAA5);
+const beigeColor = Color(0xFFFDFBF7);
