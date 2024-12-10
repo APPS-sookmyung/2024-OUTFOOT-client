d348ff9e9b65f20469c0294a8d4e4747f45f0def
seoy-54 <semi0091@naver.com>
2024-12-10 18:24:07 +0900

    [Feat#44] 친구 목록 조회 api 연결

--- outfoot/lib/main.dart
+++ outfoot/lib/main.dart
@@ -4,6 +4,8 @@ import 'package:outfoot/screens/home_page.dart';
 import 'package:outfoot/screens/login/login_page_screen.dart';
 import 'package:outfoot/screens/make_goal/make_personal_goal.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:outfoot/screens/add_friend_popup_screen.dart';
+import 'package:outfoot/screens/friend_list_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized(); // Flutter 초기화
@@ -20,10 +22,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: CheckPageFoot(
-        token: 'eyJhbGciOiJIUzM4NCJ9.eyJ1c2VybmFtZSI6IjI4YzU1OGM4LTdlODktNGFiOC04MDE5LWI0MGFmOTI3MTdlZiIsIm5pY2tuYW1lIjoi7J207ZW066a8IiwiaWF0IjoxNzMxNDg1NDcxLCJleHAiOjE3MzE0OTI2NzF9.YS6MuCXZ-dcHhAszSgKVVen3Y0OWLBDxymMSqjS9LIvzHzjRc2wNsCXMD3BcFBhu', // 임시 값
-        checkPageId: '1', // 임시 값
-      ),
+      home: FriendList(),
     );
   }
 }
